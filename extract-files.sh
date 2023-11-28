18aa471caa225f7b32447143dc47093ffe9ce9ee
dib12 <61652361+dib12@users.noreply.github.com>
2023-11-28 12:57:01 +0530

    common: Remove QtiTelephonyServicelibrary

--- extract-files.sh
+++ extract-files.sh
@@ -65,14 +65,9 @@ function blob_fixup() {
             ;;
         system_ext/etc/permissions/com.qti.dpmframework.xml \
         | system_ext/etc/permissions/dpmapi.xml \
-        | system_ext/etc/permissions/telephonyservice.xml )
-            sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/embms.xml)
+        | system_ext/etc/permissions/embms.xml \
+        | system_ext/etc/permissions/qcrilhook.xml)
             sed -i "s|/product/framework/|/system_ext/framework/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/qcrilhook.xml)
-            sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
             ;;
         system_ext/lib64/libdpmframework.so)
             grep -q "libcutils_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcutils_shim.so" "${2}"
